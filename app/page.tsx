4ca22cc98205bb85606f004e552834ed236ab8b8
David Anton <davidanton037@gmail.com>
2024-01-05 20:30:48 -0500

    Add user authentication
    Using clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <p className="text-red-500 font-bold">
-      HelloVHS
-    </p>
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton
+        afterSignOutUrl="/"
+      />
+    </div>
   )
 }
