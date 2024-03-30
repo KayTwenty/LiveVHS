49b303ea8c6065c84fa7ed999bcaf29e4d0585bb
David Anton <davidanton037@gmail.com>
2024-03-29 21:34:02 -0400

    Add stream key server input box

--- app/(dashboard)/u/[username]/keys/page.tsx
+++ app/(dashboard)/u/[username]/keys/page.tsx
@@ -1,7 +1,30 @@
-const KeysPage = () => {
+import { getSelf } from "@/lib/auth-service";
+import { Button } from "@/components/ui/button";
+import { getStreamByUserId } from "@/lib/stream-service";
+
+import { UrlCard } from "./_components/url-card";
+
+const KeysPage = async () => {
+    const self = await getSelf();
+    const stream = await getStreamByUserId(self.id);
+
+    if (!stream) {
+        throw new Error("Stream not found");
+    }
+
     return (
-        <div>
-            Keys Page
+        <div className="p-6">
+            <div className="flex items-center justify-between mb-4">
+                <h1 className="text-2xl font-bold">
+                    Keys & URL
+                </h1>
+                <Button variant="primary">
+                    Generate
+                </Button>
+            </div>
+            <div className="space-y-4">
+                <UrlCard value={stream.serverUrl} />
+            </div>
         </div>      
     );
 };
